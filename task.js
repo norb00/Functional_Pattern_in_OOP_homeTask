b1a28c339b10f88df786bb9f0a9ffb27ead7b2ef
Norbert Bagi <norbert_bagi@epam.com>
2017-01-11 14:55:46 +0100

    Add comments

--- task.js
+++ task.js
@@ -3,61 +3,73 @@ let auto = function() {
 
     publicAPI();
 
+    //static method
     function getRequiredLicenseCategory() {
         return 'B';
     }
 
     function publicAPI() {
+        //default values for AUTO
         me.maxSpeed = 100;
         me.speed = 0;
         me.speedUnit = 'km/h';
-        me.getSpeed = function() {
+        me.getSpeed = function () {
             return me.speed;
         };
-        me.setSpeed = function(_speed) {
+        me.setSpeed = function (_speed) {
             me.speed = _speed;
         };
-        me.turnEngineOn = function() {
+        me.turnEngineOn = function () {
             me.isEngineOn = true;
         };
-        me.turnEngineOff = function() {
+        me.turnEngineOff = function () {
             me.isEngineOn = false;
         };
         me.isEngineOn = false;
-        me.pressKlaxon = function() {
+        me.pressKlaxon = function () {
             console.log('Beeeep');
         };
     }
     return me;
 };
 
-let cabriolet = function() {
+let cabriolet = function () {
     let me = auto();
 
     publicAPI();
 
     function publicAPI() {
+        //extend AUTO class with some extra properties
         me.roofCollapsed = false;
-        me.collapseRoof = function() {
+        me.collapseRoof = function () {
             me.roofCollapsed = true;
         }
-        me.raiseRoof = function() {
+        me.raiseRoof = function () {
             me.roofCollapsed = false;
         }
-        me.setSpeed = function(_speed) {
+        me.setSpeed = function (_speed) {
             me.speed = _speed;
         }
 
+        //overriding default values in auto class
         me.speedUnit = 'mph';
-        me.pressKlaxon = function() {
-            console.log('New Beeeeeeep');
+        me.maxSpeed = 150;
+        me.pressKlaxon = function () {
+            console.log('Fancy Beeeeeeep');
         }
     }
 
     return me;
 }
 
+//create instances
+let a = auto();
 let c = cabriolet();
 
+console.log(a);
+console.log('auto Klaxon: ');
+a.pressKlaxon();
+
 console.log(c);
+console.log('cabriolet Klaxon: ');
 c.pressKlaxon();
